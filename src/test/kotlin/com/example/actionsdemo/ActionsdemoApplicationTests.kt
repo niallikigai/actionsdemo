4c0504245727e6225faf98901a13381f704cf8cd
niallikigai <niall@ikigaidigital.io>
2022-11-24 14:22:28 +0800

    Remove sample test not required on this project

--- src/test/kotlin/com/example/actionsdemo/ActionsdemoApplicationTests.kt
+++
@@ -1,9 +0,0 @@
-package com.example.actionsdemo
-
-import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
-
-@SpringBootTest
-class ActionsdemoApplicationTests {
-
-}
