0638c3496d584aa97c27e0c9ee71f72d63142475
niallikigai <niall@ikigaidigital.io>
2022-11-24 12:56:19 +0800

    Initial Commit

---
+++ src/main/kotlin/com/example/actionsdemo/ActionsdemoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.actionsdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ActionsdemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<ActionsdemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/actionsdemo/ActionsdemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.actionsdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ActionsdemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
