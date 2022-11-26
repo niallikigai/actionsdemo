e028e3fae5720273572c7d76788911d3f68ba0e2
niallikigai <niall@ikigaidigital.io>
2022-11-26 20:45:49 +0800

    Add contaxt loading test

---
+++ src/test/kotlin/com/example/actionsdemo/ActionsdemoApplicationTest.kt
@@ -0,0 +1,12 @@
+package com.example.actionsdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ActionsdemoApplicationTest {
+    @Test
+    fun contextLoads(){
+        
+    }
+}
